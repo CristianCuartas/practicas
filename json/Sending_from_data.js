2afb96ab8c40643c83b90f5f455957263a6552e4
CristianCuartas <cristianhz1109@gmail>
2019-04-25 18:57:59 -0500

    Form HTML a JSON

---
+++ json/Sending_from_data.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+
+  var miForm = document.getElementById('miForm');
+
+  miForm.onsubmit = function(e) {
+    e.preventDefault();
+    var formData = new FormData(miForm);
+    var jsonData = {};
+    for (var [k, v] of formData) {
+      jsonData[k] = v;
+    }
+    
+    console.log(jsonData);
+  }
+
+});
